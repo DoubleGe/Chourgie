37ddfaec8b49916e97e4e362a2de041c409b1e4f
Wesley de Graaf <535260@student.glu.nl>
2020-12-09 17:19:51 +0100

    Role Counter

---
+++ commands/roleCount.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+const lang = require("../lang.json");
+
+module.exports.run = async (bot, message, args) => {
+    var guild = await message.guild;
+    
+   if(!args[0]) return message.channel.send("Geef een rol id mee.");
+   try {guild.roles.get(args[0])}
+   catch { message.channel.send("Oeps. Er ging iets fout"); return;}
+   message.channel.send(`De Rol heeft ${guild.roles.get(args[0]).members.size} members`)
+
+}
+
+module.exports.help = {
+    name: 'rolecount',
+    desc: "Ik zeg Hallo tegen jou.",
+    cat: "Algemeen"
+}
