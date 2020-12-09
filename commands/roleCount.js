69c6defd99d12084d69389a7b40abf1ea7a04d5c
Wesley de Graaf <535260@student.glu.nl>
2020-12-09 17:21:28 +0100

    added cache to .get

--- commands/roleCount.js
+++ commands/roleCount.js
@@ -5,9 +5,9 @@ module.exports.run = async (bot, message, args) => {
     var guild = await message.guild;
     
    if(!args[0]) return message.channel.send("Geef een rol id mee.");
-   try {guild.roles.get(args[0])}
+   try {guild.roles.cache.get(args[0])}
    catch { message.channel.send("Oeps. Er ging iets fout"); return;}
-   message.channel.send(`De Rol heeft ${guild.roles.get(args[0]).members.size} members`)
+   message.channel.send(`De Rol heeft ${guild.roles.cache.get(args[0]).members.size} members`)
 
 }
 
