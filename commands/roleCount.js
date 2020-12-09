1c67fc0ede83fb2ef142c6044f91499cacafbbd6
Wesley de Graaf <535260@student.glu.nl>
2020-12-09 17:26:47 +0100

    Fix?

--- commands/roleCount.js
+++ commands/roleCount.js
@@ -3,12 +3,13 @@ const lang = require("../lang.json");
 
 module.exports.run = async (bot, message, args) => {
     var guild = await message.guild;
+    var membercount
     
    if(!args[0]) return message.channel.send("Geef een rol id mee.");
    try {guild.roles.cache.get(args[0])}
    catch { message.channel.send("Oeps. Er ging iets fout"); return;}
-   message.channel.send(`De Rol heeft ${guild.roles.cache.get(args[0]).members.size} members`)
-
+   membercount = guild.roles.cache.get(args[0]).members.size;
+   message.channel.send(`De Rol heeft ${membercount} members`);
 }
 
 module.exports.help = {
