8504dbda9af1b567bd7b43bcf0deeab299407041
Wesley de Graaf <535260@student.glu.nl>
2020-12-09 17:41:21 +0100

    Test3

--- commands/roleCount.js
+++ commands/roleCount.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.channel.send("Geef een rol id mee.");
    try {guild.roles.cache.get(args[0])}
    catch { message.channel.send("Oeps. Er ging iets fout"); return;}
-   membercount = guild.roles.cache.get(args[0].members);
+   membercount = guild.roles.cache.get(args[0].members.size);
    message.channel.send(`De Rol heeft ${membercount} members`);
 }
 
