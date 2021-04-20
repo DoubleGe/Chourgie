9780a8ef37b654f27d1ab5fa7295187bb4525060
Wesley de Graaf <535260@student.glu.nl>
2021-04-20 17:36:44 +0200

    small push

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,17 +2,20 @@ const discord = require("discord.js");
 const lang = require("../lang.json");
 
 module.exports.run = async (bot, message, args) => {
+    var userinfo = message.author;
+    if(args[0]) userinfo = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-    var userIcon = message.author.displayAvatarURL();
+
+    var userIcon = userinfo.displayAvatarURL();
         var botIcon = bot.user.displayAvatarURL();
         var serverEmbed = new discord.MessageEmbed()
             .setTitle("User info")
             .setColor("#0b84b8")
             .setThumbnail(userIcon)
             .addFields(
-                {name: "Discord gebruikers naam: ", value: message.author.username},
-                {name: "Jij bent gejoined op: ", value: message.member.joinedAt},
-                {name: "Discord gejoined op: ", value: message.author.createdAt}
+                {name: "Discord gebruikers naam: ", value: userinfo.username},
+                {name: "Jij bent gejoined op: ", value: userinfo.joinedAt},
+                {name: "Discord gejoined op: ", value: userinfo.createdAt}
             )
             .setFooter("Chourgie® ", botIcon)
             .setTimestamp();
